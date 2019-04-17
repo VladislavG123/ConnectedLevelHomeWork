3f19f72235db8842a3f50a5f59353261181044c5
Владислав Горохов <gbg.0101@mail.ru>
2019-04-17 16:16:37 +0600

    Initial

---
+++ ConnectedLevelHomeWork/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectedLevelHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["appConnection"].ConnectionString; ;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+
+                command.CommandText = "create table [Group](Id int primary key, Name nvarchar(256) not null check(len(Name)>0))";
+
+                var transaction = connection.BeginTransaction();
+                command.Transaction = transaction;
+
+                transaction.Commit();
+                command.ExecuteReader();
+            }
+        }
+    }
+}
